fe561f8eaa5d4583f9459015ab0d1053cb9930af
Alejo Dieguez <alejodieguez2012@gmail.com>
2023-03-27 18:19:01 -0300

    changes

--- assets/donations.js
+++ assets/donations.js
@@ -1,10 +1,26 @@
-const quantity = document.querySelectorAll("button");
-function donations() {
-  console.log("holas");
-  console.log(quantity);
-  quantity.forEach((quant) => {
-    quant.addEventListener("click", () => {
-      console.log(quant.innerHTML);
+if (document.readyState) {
+  setTimeout(() => {
+    const buttons = document.querySelectorAll(".donations__button");
+    const totalPriceElement = window.Rebuy.Cart.subtotal();
+    console.log(totalPriceElement + 100);
+    const prices = [1, 2, 5, 10];
+
+    buttons.forEach((button) => {
+      button.price = prices[Math.floor(Math.random() * prices.length)];
     });
-  });
+
+    buttons.forEach((button) => {
+      button.addEventListener("click", () => {
+        updatePrice(button.price);
+      });
+    });
+
+    function updatePrice(price) {
+      // const totalPrice = Number(
+      //   totalPriceElement.textContent.replace(/[^0-9.-]+/g, "")
+      // );
+      const newTotalPrice = (totalPriceElement + price).toFixed(2);
+      totalPriceElement.textContent = "$" + newTotalPrice;
+    }
+  }, 2000);
 }
