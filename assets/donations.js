7fc8635c56d8c23bf6b13f091c2ab69cb06eec79
Alejo Dieguez <alejodieguez2012@gmail.com>
2023-03-29 15:52:36 -0300

    changes

--- assets/donations.js
+++ assets/donations.js
@@ -1,26 +1,42 @@
-if (document.readyState) {
-  setTimeout(() => {
-    const buttons = document.querySelectorAll(".donations__button");
-    const totalPriceElement = window.Rebuy.Cart.subtotal();
-    console.log(totalPriceElement + 100);
-    const prices = [1, 2, 5, 10];
+setTimeout(() => {
+  // const buttons = document.querySelectorAll(".donations__button");
+  // const totalPriceElement = window.Rebuy.Cart.subtotal();
+  // console.log(totalPriceElement + 100);
+  // const prices = [1, 2, 5, 10];
 
-    buttons.forEach((button) => {
-      button.price = prices[Math.floor(Math.random() * prices.length)];
-    });
+  // buttons.forEach((button) => {
+  //   button.price = prices[Math.floor(Math.random() * prices.length)];
+  // });
 
-    buttons.forEach((button) => {
-      button.addEventListener("click", () => {
-        updatePrice(button.price);
-      });
-    });
+  // buttons.forEach((button) => {
+  //   button.addEventListener("click", () => {
+  //     updatePrice(button.price);
+  //   });
+  // });
 
-    function updatePrice(price) {
-      // const totalPrice = Number(
-      //   totalPriceElement.textContent.replace(/[^0-9.-]+/g, "")
-      // );
-      const newTotalPrice = (totalPriceElement + price).toFixed(2);
-      totalPriceElement.textContent = "$" + newTotalPrice;
+  // function updatePrice(price) {
+  //   // const totalPrice = Number(
+  //   //   totalPriceElement.textContent.replace(/[^0-9.-]+/g, "")
+  //   // );
+  //   const newTotalPrice = (totalPriceElement + price).toFixed(2);
+  //   totalPriceElement.textContent = "$" + newTotalPrice;
+  // }
+  const itemsContainer = document.querySelector(".rebuy-cart__flyout-items");
+  const buttons = document.querySelectorAll(".rebuy-button");
+  const observer = new MutationObserver(function (itemsList, observer) {
+    if (itemsList.some((item) => item.target === itemsContainer)) {
+      if (itemsContainer != null) {
+        buttons.forEach((button) => {
+          button.classList.add("button-active");
+          button.classList.remove("button-inactive");
+        });
+      } else {
+        buttons.forEach((button) => {
+          button.classList.add("button-inactive");
+          button.classList.remove("button-active");
+        });
+      }
     }
-  }, 2000);
-}
+  });
+  observer.observe(itemsContainer, { attributes: true, childList: true });
+}, 3000);
