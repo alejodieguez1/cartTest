25abd1dcb962e605594de31b5cad85f821ea6455
Alejo Dieguez <alejodieguez2012@gmail.com>
2023-03-30 18:04:16 -0300

    fixed styling + buttons functionality

--- assets/itemLimits.js
+++ assets/itemLimits.js
@@ -13,4 +13,14 @@ document.addEventListener("rebuy:cart.change", (event) => {
       .querySelector(".limit__error-modal")
       .classList.replace("show", "hide");
   });
+  const cartBtns = document.querySelectorAll(".rebuy-button");
+  setTimeout(() => {
+    cartBtns.forEach((btn) => {
+      if (window.Rebuy.Cart.items().length > 0) {
+        btn.classList.add("btn-inactive");
+      } else if (window.Rebuy.Cart.items().length == 0) {
+        btn.classList.remove("btn-inactive");
+      }
+    });
+  }, 500);
 });
