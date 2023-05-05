0b8e8ed5b3b76b3f5760084ae0beca0ca94bd46d
Alejo Dieguez <alejodieguez2012@gmail.com>
2023-05-05 14:24:00 -0300

    implemented web component

---
+++ assets/web-components.js
@@ -0,0 +1,52 @@
+class MoneyFormatter extends HTMLElement {
+  constructor() {
+    super();
+    this.timer = null;
+    this.timeout = 300;
+    this.mutationObserver = null;
+  }
+
+  connectedCallback() {
+    this.formatMoney();
+    this.observeChanges();
+  }
+
+  disconnectedCallback() {
+    this.disconnectObserver();
+  }
+
+  observeChanges() {
+    const options = { childList: true, subtree: true };
+    this.mutationObserver = new MutationObserver((event) => {
+      // this.formatMoney();
+      console.log(event);
+    });
+    this.mutationObserver.observe(this, options);
+  }
+
+  disconnectObserver() {
+    if (this.mutationObserver) {
+      this.mutationObserver.disconnect();
+    }
+  }
+
+  debounce(func, timeout) {
+    return (...args) => {
+      clearTimeout(this.timer);
+      this.timer = setTimeout(() => {
+        func.apply(this, args);
+      }, timeout);
+    };
+  }
+
+  formatMoney() {
+    const formattedMoney = window.Rebuy.Cart.formatMoney(
+      this.dataset.value,
+      "{{ amount_no_decimals }}"
+    );
+    this.innerHTML = formattedMoney;
+    console.log();
+  }
+}
+
+customElements.define("money-formatter", MoneyFormatter);
